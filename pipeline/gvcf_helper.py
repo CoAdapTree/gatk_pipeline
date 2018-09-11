d646989940650cdeaced545a31d493d72d35e84f
Brandon Lind <lindb@cedar5.cedar.computecanada.ca>
2018-09-11 16:23:13 -0700

    mem efficiency

--- pipeline/gvcf_helper.py
+++ pipeline/gvcf_helper.py
@@ -1,3 +1,7 @@
+### FIX
+# at some point, have the jobID/mem as an input arg instead of script
+###
+
 ###
 # usage: gvcf_helper.py /path/to/fastq.gz/folder/
 
@@ -29,16 +33,21 @@ os.chdir(DIR)
 workingdir = op.join(DIR,'workingdir')
 if not op.exists(workingdir):
     os.makedirs(workingdir)
+    
+# get job info and current memory limit
+jobid  = os.popen('echo ${SLURM_JOB_ID}').read().replace("\n","")
+jobmem = os.popen("sacct -j %s | grep 'lindb'" % jobid).read()
+jobmem = int([x for x in jobmem.split() if 'mem' in x][0].split(",")[1].split('=')[1].replace("M",""))
 
+# get list of remaining gatk calls
 shfiles = [f for f in fs(DIR) if f.endswith('.sh')]
-shuffle(shfiles) # so I don't have to code something in to deal with/avoid multiple gvcf_helpers 'helping' the same sh file
-# shuffling won't be perfect, esp when len(shfiles) == <a smallish number>, but oh well
+shuffle(shfiles) 
 
 # run commands until I run out of time
 print('running gvcf_helper.py')
 for s in shfiles:
+    print (s)
     if op.exists(s):
-#         print s # so that rescheduler can find it (should print to stdout in the sh file's .out file)
         reservation = op.join(workingdir,op.basename(s))
         try:
             shutil.move(s,reservation) # so that other jobs don't rewrite
@@ -46,8 +55,19 @@ for s in shfiles:
             print('could not move shfile %s' % s)
             print('to reservation %s' % reservation)
             continue
-        print(reservation) # so that rescheduler can find it (should print to stdout in the sh file's .out file)
-        o = open(reservation,'r').readlines()
+        print(reservation)
+        with open(reservation,'r') as O:
+            o = O.readlines()
+#         o = open(reservation,'r').readlines()
+        
+        # only continue to run jobs that fit in the same memory allocation (dont waste resources if its going to fail)
+        mem = int([x for x in o if 'mem' in x][0].split("=")[1].replace("M\n",""))
+        if mem > jobmem:
+            print ('file exceeds mem limit')
+            shutil.move(reservation,s) # put the job back in the queue
+            continue
+        
+        print ('file is ok to proceed')
         for line in o:
             if line.startswith('gatk'):
                 cmd = line.replace('\n','')
