6507de5aa49e6d5a0293429020f67b0c8f36bd3b
Brandon Lind <lindb@cedar5.cedar.computecanada.ca>
2018-09-17 10:57:15 -0700

    combine gvcfs

--- pipeline/03_combine-gvcfs.py
+++ pipeline/03_combine-gvcfs.py
@@ -9,6 +9,9 @@ import os
 from os import path as op
 from os import listdir
 import pickle
+import numpy as np
+def uni(mylist):
+    return (np.unique(mylist).tolist())
 def ls(DIR):
     return sorted([f for f in listdir(DIR)])
 def fs (DIR):
@@ -16,36 +19,75 @@ def fs (DIR):
 ### 
 
 ### args
-thisfile, fqdir, ref, samp = sys.argv
+thisfile, parentdir = sys.argv
+if parentdir.endswith("/"):
+    parentdir = parentdir[:-1]
 ###
 
+### reqs
+samp2pool = pickle.load(open(op.join(parentdir,'samp2pool.pkl'),'rb'))
+rginfo    = pickle.load(open(op.join(parentdir,'rginfo.pkl'),'rb'))
+poolsamps = pickle.load(open(op.join(parentdir,'poolsamps.pkl'),'rb')) #key=pool val=list(sampnames)
+poolref   = pickle.load(open(op.join(parentdir,'poolref.pkl'),'rb'))   #key=pool val=/path/to/ref.fa
+pools = uni(list(samp2pool.values()))
+samps = list(rginfo.keys())
+###
 
+pooldirs = []
+for p in pools:
+    pooldir = op.join(parentdir,p)
+    assert op.exists(pooldir)
+    pooldirs.append(pooldir)
 
-gvcfdir = op.join(fqdir,'vcfs')
-comdir  = op.join(fqdir,'combined_gvcfs')
-
-if not op.exists(comdir):
-    os.makedirs(comdir)
-    
-files = [f for f in fs(gvcfdir) if samp in f]
-print 'len(files) =',len(files)
-
-text = '''#!/bin/bash
-#SBATCH --time=02:59:00
+shfiles = []
+for pdir in pooldirs:
+    pool    = op.basename(pdir)
+    ref     = poolref[pool]
+    gvcfdir = op.join(pdir,'vcfs')
+    assert op.exists(gvcfdir)
+    comdir  = op.join(pdir,'combined_gvcfs')
+    shdir   = op.join(pdir,'shfiles/combine_gvcfs')
+    for x in [comdir,shdir]:
+        if not op.exists(x):
+            os.makedirs(x)
+        
+    # get list of gvcf files for each samp
+    for samp in poolsamps[pool]:
+        sampfiles  = [f for f in fs(gvcfdir) if samp in f and f.endswith('.gz')]
+        outfile    = op.join(comdir,'%s_combined.g.vcf.gz' % samp)
+        variantcmd = '--variant ' + ' --variant '.join([x for x in sorted(sampfiles)])
+        filE = op.join(shdir,'%s.sh' % samp)
+        text       = '''#!/bin/bash
+#SBATCH --time=11:59:00
 #SBATCH --nodes=1
-#SBATCH --mem=8000M
+#SBATCH --mem=30000M
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --job-name=%s%s%s
+#SBATCH --job-name=combine_%(samp)s
 #SBATCH --export=all
-#SBATCH --output=gvcf%s_%%j.out 
-#SBATCH --mail-user=lindb@vcu.edu
+#SBATCH --output=combine-gvcf_%(samp)s_%%j.out 
+
+# for debugging
+echo $0
+echo %(filE)s
 
 source $HOME/.bashrc
-module load gatk/4.0.0.0
+module load gatk/4.0.8.1
 
-gatk CombineGVCFs 
-'''
+gatk CombineGVCFs -R %(ref)s %(variantcmd)s -O %(outfile)s
+
+''' % locals()
+        with open(filE,'w') as o:
+            o.write("%s" % text)
+        shfiles.append(filE)
+        print(filE)
+        
+
+for s in shfiles:
+    os.chdir(op.dirname(s))
+    os.system('sbatch %s' % s)
+
+        
 
 
 
