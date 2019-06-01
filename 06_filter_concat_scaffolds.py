88afb215b875a926f9a407442bd69cc5b9de9fe3
Brandon Lind <lindb@vcu.edu>
2019-06-01 09:55:23 -0700

    revamp

--- 06_filter_concat_scaffolds.py
+++ 06_filter_concat_scaffolds.py
@@ -1,15 +1,21 @@
-### usage
-# python 04_concat_scaffolds.py /path/to/parentdir/used/in/00_start-pipeline/command 
-### 
+"""
+# usage
+# python 04_concat_scaffolds.py /path/to/parentdir
+# 
 
-### purpose
-# instead of calling snps when combining some of our pools, call snps for indiviual pools
-###
+# purpose
+# concat all genotyped files for each pool into a vcf file if all files are ready
+# filter this vcf file for quality depth, fisher strand, mapping quality, \
+MQRankSum, MAF < 0.05, no less than 75% missing data
+#
 
-### FIX
-# I've used 'kit' as a key word that is in all of the library/pool names, this should be changed if not the case
-# see creation of combdict
-###
+# assumes
+# intervals files used to make scatter files in 04_scatter.py will sort properly
+# ie in order as they appear of the reference, which is important for concatenating vcfs
+#
+# GQ for samps should be filtered afterwards and then again for missing data after removing samps
+# if I used "--genotypeFilterExpression GQ < 20" I could miss SNPs that have > 75% of samps with GQ >=20
+"""
 
 ### imports
 import sys
@@ -18,87 +24,105 @@ from os import path as op
 from os import listdir
 import pickle
 import numpy as np
-def uni(mylist):
-    return (np.unique(mylist).tolist())
-def ls(DIR):
-    return sorted([f for f in listdir(DIR)])
-def fs (DIR):
-    return sorted([op.join(DIR,f) for f in ls(DIR)])
-def createdirs(dirs):
-    for d in dirs:
-        if not op.exists(d):
-            os.makedirs(d)
+from coadaptree import fs, createdirs, pklload, get_email_info
 ### 
 
 ### args
 thisfile, parentdir = sys.argv
 if parentdir.endswith("/"):
     parentdir = parentdir[:-1]
-poolref = pickle.load(open(op.join(parentdir,'poolref.pkl'),'rb'))
+poolref = pklload(op.join(parentdir, 'poolref.pkl'))
+email_info = get_email_info(parentdir, 'concat')
 ###
 
 ### dirs
-shdir   = op.join(parentdir,'shfiles/concat')
-catdir  = op.join(parentdir,'concatenated_vcfs')
-filtdir = op.join(parentdir,'filtered_snps')
+shdir   = op.join(parentdir, 'shfiles/concat')
+catdir  = op.join(parentdir, 'concatenated_vcfs')
+filtdir = op.join(parentdir, 'filtered_snps')
 createdirs([shdir,catdir,filtdir])
 ###
 
 # get the snpfiles
-snpdir = op.join(parentdir,'snps')
-allfiles = fs(snpdir)
-snpfiles = [f for f in fs(snpdir) if f.endswith('.gz') and 'snp' in op.basename(f) and f.replace('.gz','.gz.tbi') in allfiles]
+snpdir = op.join(parentdir, 'snps')
+snpfiles = [f.replace('.tbi', '') for f in fs(snpdir) if 'snp' in op.basename(f) and f.endswith('.tbi')]
 os.system('echo "len(snpfiles) = %s"' % str(len(snpfiles)))
 
-# sort snpfiles by combo lib
+# sort snpfiles by pool
+pools = list(poolref.keys())
 combdict = {}
 for i,snp in enumerate(snpfiles):
-    lib = "---".join([x for x in op.basename(snp).split("-") if 'kit' in x])
-    if not lib in combdict:
-        combdict[lib] = []
-    combdict[lib].append(snp)
-os.system('echo there are %s keys in combdict' % str(len(combdict.keys())))
-for k,kfiles in combdict.items():
-    os.system('echo %s %s' % (k,str(len(kfiles))))
+    for p in pools:
+        if p in op.basename(snp):
+            pool = p
+            break
+    if not pool in combdict:
+        combdict[pool] = []
+    combdict[pool].append(snp)
+    del pool  # will cause script to error if pool isn't found in snpfile
+for pool,poolfiles in combdict.items():
+    os.system(f'echo there are {len(poolfiles)} snpfiles for {pool} pool')
+
+# get the expected number of snpfiles per pool
+expected = {}
+for pool,ref in poolref.items():
+    scafdir = op.join(op.dirname(ref), 'intervals')
+    scaffiles = [f for f in fs(scafdir) if f.endswith('.list')]
+    os.system(f'echo found {len(scaffiles)} intervalfiles for {pool}')
+    expected[pool] = len(scaffiles)
+
 # write the sh files
 shfiles = []
 fcats   = []
-for lib in combdict.keys():
-    if len(combdict[lib]) in [50289]: # for now this is for Doug fir, will need to change for other spp
-        os.system('echo creating sbatch file')
-        catout   = op.join(catdir,"%s_concatenated_snps.vcf.gz" % lib)
-        filtout  = op.join(filtdir,"%s_filtered_concatenated_snps.vcf.gz" % lib)
-        firstlib = lib.split("---")[0]
-        ref      = poolref[firstlib]
-        # I should have made scaffols be zfill(4) not zfill(3)
-        files    = " ".join([snp for snp in sorted(combdict[lib]) if '1000' not in snp])  # I can remove the if-condition once we're done with the pseudo ref
-        # (bcftools needs the input files to be sorted)
-        files    = files + ' %s ' % [snp for snp in combdict[lib] if '1000' in snp][0] 
-        text = '''#!/bin/bash
+for pool,files in combdict.items():
+    if len(files) == expected[pool]:
+        os.system(f'echo creating sbatch file for {pool}')
+        catout = op.join(catdir, f"{pool}_concatenated_snps.vcf.gz")
+        filtout = op.join(filtdir, f"{pool}_filtered_concatenated_snps.vcf.gz")
+        if not op.exists(filtout): # so I can run this when files begin to finish from 05_genotype.py
+            nomissing = filtout.replace(".vcf.gz", "_no-missing.vcf.gz")
+            tablefile = nomissing.replace(".vcf.gz", "_table.txt")
+            ref = poolref[pool]
+            files = ' '.join(sorted(files))
+            text = f'''#!/bin/bash
 #SBATCH --time=11:59:59
 #SBATCH --mem=15000M
 #SBATCH --nodes=1
 #SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1
-#SBATCH --job-name=%(lib)s-concat
-#SBATCH --output=%(lib)s-concat_%%j.out 
-#SBATCH --mail-user=lindb@vcu.edu
-#SBATCH --mail-type=FAIL
+#SBATCH --job-name={pool}-concat
+#SBATCH --output={pool}-concat_%j.out 
+{email_info}
+
+source $HOME/.bashrc
+export _JAVA_OPTIONS="-Xms256m -Xmx13g"
+echo $0
 
 module load bcftools/1.9
+echo "CONCATENATING FILES"
+bcftools concat {files} -O z -o {catout} --threads 32
+module unload bcftools
 
-bcftools concat %(files)s -O z -o %(catout)s --threads 32
+module load gatk/4.1.0.0
+echo -e "\nFILTERING VARIANTS"
+gatk IndexFeatureFile -F {catout}
+gatk VariantFiltration -R {ref} -V {catout} -O {filtout} --filter-expression \
+"QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || AF < 0.05 || AF > 0.95" \
+--filter-name "coadaptree_filter"
+module unload gatk
 
-module load gatk/4.0.8.1
+module load vcftools/0.1.14
+echo -e "\nFILTERING MISSING DATA"
+vcftools --gzvcf {filtout} --max-missing 0.75 --recode --recode-INFO-all --out {nomissing}
+module unload vcftools
 
-gatk IndexFeatureFile -F %(catout)s
+module load gatk/4.1.0.0
+echo -e "\nVARIANTS TO TABLE"
+gatk VariantsToTable --variant {nomissing} -F CHROM -F POS -F REF -F ALT -F AF -F DP -F QD \
+-F FS -F MQ -F MQRankSum -F ReadPosRankSum -GF AD -GF DP -GF GQ -GF GT -GF SB -O {tablefile}
 
-gatk VariantFiltration -R %(ref)s -V %(catout)s -O %(filtout)s --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5" --filter-name "coadaptree_filter"
-
-''' % locals()
-        file = op.join(shdir,"%s-concat.sh" % lib)
-        if not op.exists(filtout): # so I can run this when files begin to finish from 03_genotype.py
+'''
             if not op.exists(filtout.replace(".gz",".gz.tbi")):
+                file = op.join(shdir,"%s-concat.sh" % pool)
                 with open(file,'w') as o:
                     o.write("%s" % text)
                 shfiles.append(file)
@@ -115,10 +139,11 @@ gatk VariantFiltration -R %(ref)s -V %(catout)s -O %(filtout)s --filter-expressi
                     for line in lines:
                         o.write("%s\n" % line)
 
-# os.chdir(shdir)
-# for sh in shfiles:
-#     os.system('echo %s' % sh)
-#     os.system('sbatch %s' % sh)
-[print(sh) for sh in shfiles]
-[print(f) for f in fcats]
-        
+os.chdir(shdir)
+for sh in shfiles:
+    os.system('echo %s' % sh)
+    os.system('sbatch %s' % sh)
+
+print('here are the fcats:')
+for f in fcats:
+    print(f)
